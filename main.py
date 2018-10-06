5e3640ac4803b3198f34bc7277d7f893ec62c300
tranthanh96 <tranthanh96hd@gmail.com>
2018-10-06 11:38:16 +0700

    update preprocess data

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
 from model import BEGAN
+import numpy as np 
+
+X = np.load('/media/HDD_2T/data/VGG-Faces/VGG-Faces-ThanhTM/mini_vgg_data.npy')
+X = X*2-1
+
 
 model = BEGAN()
-model.test_model()
+model.fit(X)
